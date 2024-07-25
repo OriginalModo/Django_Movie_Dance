8c26eb72bc4ed39b60b63ecd74b97779a6d35d5b
PK111 <1@>
2024-07-25 16:14:13 +0300

    regex SQL

--- movie_app/models.py
+++ movie_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+# python manage.py shell_plus --print-sql
+class Movie(models.Model):
+    name = models.CharField(max_length=40)
+    rating = models.IntegerField()
+    year = models.IntegerField(null=True)
+    budget = models.IntegerField(default=1_000_000)
+
+
+    def __str__(self):
+        return f'{self.name} - {self.rating}%'
+
+
+
+
